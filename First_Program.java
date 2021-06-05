d03993c02400ed8cf1040e265c9695bef22b41ef
Shweta709 <81040434+Shweta709@users.noreply.github.com>
2021-06-05 22:41:01 +0530

    regards

---
+++ First_Program.java
@@ -0,0 +1,16 @@
+
+public class First_Program {
+	/* Global variable */
+	static int a=10;          //static variable
+	String name="Shweta";     //instance variable
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//local variable
+		First_Program obj1 = new First_Program();
+		System.out.println(a);
+		System.out.println(obj1.name);
+		
+	}
+
+}
